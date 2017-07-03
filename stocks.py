1ff2baf5f284df6635d21842bbc39133e0fa5c0f
Matthew Williams <matt_williams_5@hotmail.com>
2017-07-03 10:34:47 -0400

    Added options lookup for stock

--- stocks.py
+++ stocks.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 import pandas as pd
-#from pandas_datareader import data as web
 import pandas_datareader.data as web
 import datetime
-
+from pandas_datareader.data import Options
+from pandas_datareader import wb
 
 # creating start and stop times for our stock
 start = datetime.datetime(2016,1,1)
@@ -13,7 +13,7 @@ end = datetime.date.today()
 
 #Retrieving user input for historical data or quotes for stock
 
-ans = input("Enter 1 for historical data or 2 for quote: ")
+ans = input("Enter 1 for historical data, 2 for quote or 3 for options: ")
 
 if ans == 1:
     stock = raw_input("Please enter the stock you want historical data from: ")
@@ -23,5 +23,8 @@ elif ans == 2:
     quote = raw_input("Please enter the stock you want todays quote from: ")
     q = web.get_quote_google([quote, 'GOOG'])
     print(q)
-
-
+elif ans == 3:
+    opts = raw_input("Please enter stock you need options results for: ")
+    go_opts = Options(opts, 'google')
+    data = go_opts.get_options_data(expiry=go_opts.expiry_dates[0])
+    print(data.iloc[0:5, 0:5])
