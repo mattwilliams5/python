efcbdf6bd93747ab7598f50bcd5ce7c8d6e74efe
Matt <matt_williams_5@hotmail.com>
2017-08-08 11:29:23 -0400

    Update mapit.py

--- mapit.py
+++ mapit.py
@@ -3,13 +3,11 @@
 import webbrowser, pyperclip
 from sys import argv
 
-# mapit - Launches a map in the browswer using an address from the command line
-# or clipboard (Must install pyperclip pip3 install pyperclip)
-
-def location(address):
-    # Get the address
+if len(argv) > 1:
+    # Get address from command line.
     address = ' '.join(argv[1:])
-    if argv < 1:
-        # Get address from clipboard
-        address = pyperclip.paste()
-        webbrowser.open('https://www.google.com/maps/place/' + address)
+else:
+    # Get address from clipboard.
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
